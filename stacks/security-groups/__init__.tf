3de152253e8cb378be80e36a3e90fe548e0abeea
boyapatihemanth <boyapatihemanth@gmail.com>
2022-09-05 21:11:24 +0100

    change to versions

--- stacks/security-groups/__init__.tf
+++ stacks/security-groups/__init__.tf
@@ -1,16 +1,17 @@
 terraform {
-  backend "s3" {
-    bucket = "boyapatihemanthnl"
-    key = "sg-demo.tfstate"
-    workspace_key_prefix = "tfstate/sg"
-    region = "eu-west-1"
-  }
+  #backend "s3" {
+  #  bucket = "boyapatihemanthnl"
+  #  key = "sg-demo.tfstate"
+  #  workspace_key_prefix = "tfstate/sg"
+  #  region = "eu-west-1"
+  #}
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "4.28.0"
     }
   }
+  required_version ~> "1.2.0"
 }
 
 provider "aws" {
